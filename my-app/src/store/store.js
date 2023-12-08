892537936f6564a448abb77b64ae739290a6afc7
alptugozates <omeralptugozates@gmail.com>
2023-12-08 19:30:47 +0300

    redux-logger kurar

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import logger from "redux-logger";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 function reducer() {
   return;
