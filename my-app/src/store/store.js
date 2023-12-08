16811351eade4f6d46ac936caae40ce6b3634131
alptugozates <omeralptugozates@gmail.com>
2023-12-08 19:24:26 +0300

    store oluşturur, thunk kurar

---
+++ my-app/src/store/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { thunk } from "redux-thunk";
+
+export const store = createStore(reducer, applyMiddleware(thunk));
+
+function reducer() {
+  return;
+}
