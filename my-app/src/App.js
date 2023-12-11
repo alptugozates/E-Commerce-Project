a9e90a5579a8b3f41fe1f5a54b42162d132b7831
alptugozates <omeralptugozates@gmail.com>
2023-12-11 23:00:23 +0300

    shopcards app e ekler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import ShopCards from "./components/ShopCards";
 
 function App() {
   return (
     <div>
       <Header />
       <HomePage />
+      <ShopCards />
       <Footer />
     </div>
   );
