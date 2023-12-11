f9d10fd85f17d28073a6681d0c55c96f6751b885
alptugozates <omeralptugozates@gmail.com>
2023-12-11 16:11:28 +0300

    footer component olusturur

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="h-auto self-stretch">
+      <div className="flex jutify-center items-center px-52">
+        <div className="container flex items-start flex-col shrink-0 py-10 px-0">
+          <div className="row flex items-center gap-[37rem] ">
+            <div className="col-md-3 flex flex-col items-start gap-3.5 ">
+              <nav className="flex items-center py-4 px-20">
+                <a className="font-montserrat font-bold text-2xl tracking-[0.00625rem] text-[#252B42] ">
+                  Bandage
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
