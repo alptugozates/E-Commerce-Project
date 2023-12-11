41c9387ba6f3b5b6a1d2f33547ddabea9e7602f6
alptugozates <omeralptugozates@gmail.com>
2023-12-11 21:25:27 +0300

    div içerisinde img'ları düzenler

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -6,11 +6,26 @@ import faBrands5 from "../foto/fa-brands-5.png";
 import faBrands6 from "../foto/fa-brands-6.png";
 const HomePage = () => {
   return (
-    <div className="clients-1 flex px-52 w-full justify-center items-center">
+    <div className="clients-1 flex px-52 w-full justify-center items-center bg-[#FFF] ">
       <div className="container flex flex-col items-center shrink-0 ">
-        <div className="row flex justify-center items-center gap-8 px-0 py-14">
+        <div className="row flex justify-center items-center gap-32 px-0 py-28">
           <div className="col-md-2 flex flex-col items-center">
-            <img src={faBrands1}></img>
+            <img src={faBrands1} />
+          </div>
+          <div className="col-md-2 flex flex-col items-center">
+            <img src={faBrands2} />
+          </div>
+          <div className="col-md-2 flex flex-col items-center">
+            <img src={faBrands3} />
+          </div>
+          <div className="col-md-2 flex flex-col items-center">
+            <img src={faBrands4} />
+          </div>
+          <div className="col-md-2 flex flex-col items-center">
+            <img src={faBrands5} />
+          </div>
+          <div className="col-md-2 flex flex-col items-center">
+            <img src={faBrands6} />
           </div>
         </div>
       </div>
