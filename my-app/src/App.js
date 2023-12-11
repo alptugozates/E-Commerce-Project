db627137e0439e5b32f0736866672f0668a8a5f9
alptugozates <omeralptugozates@gmail.com>
2023-12-11 20:39:55 +0300

    app'e homePage ekler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div>
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
