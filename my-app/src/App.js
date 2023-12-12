e8abf75ebb70a4e6e30b4af8a0fbcf6890cb846c
alptugozates <omeralptugozates@gmail.com>
2023-12-12 21:32:56 +0300

    componentleri render eder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomePage from "./components/HomePage";
+import ClientsContent from "./components/ClientsContent";
 import ProductCards from "./components/ProductCards";
 import ShopCards from "./components/ShopCards";
-
+import Content from "./components/Content";
+import Features from "./components/Features";
 function App() {
   return (
     <div>
       <Header />
-      <HomePage />
+      <ClientsContent />
       <ShopCards />
       <ProductCards />
+      <Content />
+      <Features />
       <Footer />
     </div>
   );
