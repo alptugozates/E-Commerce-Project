65d9a246b5cf1eebb94953f6cc5fee91f17c5550
alptugozates <omeralptugozates@gmail.com>
2023-12-12 22:05:25 +0300

    Componentleri HomePage'de toplar app'de render eder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import ClientsContent from "./components/ClientsContent";
-import ProductCards from "./components/ProductCards";
-import ShopCards from "./components/ShopCards";
-import Content from "./components/Content";
-import Features from "./components/Features";
+import HomePage from "./components/HomePage";
 function App() {
   return (
     <div>
-      <Header />
-      <ClientsContent />
-      <ShopCards />
-      <ProductCards />
-      <Content />
-      <Features />
-      <Footer />
+      <HomePage />
     </div>
   );
 }
---
+++ my-app/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import ClientsContent from "./ClientsContent";
+import Content from "./Content";
+import Features from "./Features";
+import Footer from "./Footer";
+import Header from "./Header";
+import ProductCards from "./ProductCards";
+import ShopCards from "./ShopCards";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <ClientsContent />
+      <ShopCards />
+      <ProductCards />
+      <Content />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+export default HomePage;
