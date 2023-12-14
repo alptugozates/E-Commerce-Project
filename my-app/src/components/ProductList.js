fbab67387ea9d9d6e3e5dce949357493bd076493
alptugozates <omeralptugozates@gmail.com>
2023-12-14 03:15:50 +0300

    ProductList componenti olusturur

---
+++ my-app/src/components/ProductList.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const ProductList = () => {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+};
+export default ProductList;
