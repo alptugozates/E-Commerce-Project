850030b07130c373afb3e6812d267c1f8e780b55
alptugozates <omeralptugozates@gmail.com>
2023-12-14 03:15:18 +0300

    router ile sarıldı

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/products">
+          <ProductList />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
