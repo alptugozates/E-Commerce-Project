58952209ada34beb536e66b3e4f5a966b596deb6
alptugozates <omeralptugozates@gmail.com>
2023-12-14 23:22:30 +0300

    productsData olusturur

---
+++ my-app/src/data/ProductsData.js
@@ -0,0 +1,111 @@
+import product1 from "../foto/foto ProductList/product-1.png";
+import product2 from "../foto/foto ProductList/product-2.png";
+import product3 from "../foto/foto ProductList/product-3.png";
+import product4 from "../foto/foto ProductList/product-4.png";
+import product5 from "../foto/foto ProductList/product-5.png";
+import product6 from "../foto/foto ProductList/product-6.png";
+import product7 from "../foto/foto ProductList/product-7.png";
+import product8 from "../foto/foto ProductList/product-8.png";
+import product9 from "../foto/foto ProductList/product-9.png";
+import product10 from "../foto/foto ProductList/product-10.png";
+import product11 from "../foto/foto ProductList/product-11.png";
+import product12 from "../foto/foto ProductList/product-12.png";
+
+export const productsData = [
+  {
+    id: 1,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product1,
+  },
+  {
+    id: 2,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product2,
+  },
+  {
+    id: 3,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product3,
+  },
+  {
+    id: 4,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product4,
+  },
+  {
+    id: 5,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product5,
+  },
+  {
+    id: 6,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product6,
+  },
+  {
+    id: 7,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product7,
+  },
+  {
+    id: 8,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product8,
+  },
+  {
+    id: 9,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product9,
+  },
+  {
+    id: 10,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product10,
+  },
+  {
+    id: 11,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product11,
+  },
+  {
+    id: 12,
+    name: "Graphic Design",
+    description: "English Department",
+    price: "$16.48",
+    price2: "$6.48",
+    image: product12,
+  },
+];
