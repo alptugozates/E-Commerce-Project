8d6ac58c55a6c9d0c5fb235df3c489fb6ab3a42a
alptugozates <omeralptugozates@gmail.com>
2023-12-15 14:02:46 +0300

    react slick kurar, slider component olsuturur

---
+++ my-app/src/components/MySlider.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import FluidContent from "./FluidContent";
+import sliderImg from "../foto/hero-slider.jpg";
+import AnotherFluidContent from "./AnotherFluidContent";
+
+const MySlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <FluidContent alt="Slide 1" />
+      </div>
+      <div>
+        <AnotherFluidContent alt="Slide 2" />
+      </div>
+    </Slider>
+  );
+};
+
+export default MySlider;
