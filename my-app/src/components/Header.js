00c4a9e7bd8717845d0f682658525f3c45d0d796
alptugozates <omeralptugozates@gmail.com>
2023-12-15 14:03:52 +0300

    home yönlendirmesi ekler

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -94,7 +94,7 @@ const Header = () => {
             <nav className="flex items-start gap-6">
               <li className="flex w-11 h-6 justify-center items-center">
                 <a
-                  href=""
+                  href="/"
                   className="shrink-0 text-custom-gray font-bold font-montserrat tracking-[0.0125rem] text-base "
                 >
                   Home
