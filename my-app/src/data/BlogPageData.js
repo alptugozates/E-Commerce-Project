0918ea8479d3a351a6eb30a5d9679939b997ca51
alptugozates <omeralptugozates@gmail.com>
2023-12-16 19:14:13 +0300

    BlogPageData olusturur

---
+++ my-app/src/data/BlogPageData.js
@@ -0,0 +1,63 @@
+import blogCart1 from "../foto/blogFoto/blogCart-1.png";
+import blogCart2 from "../foto/blogFoto/blogCart-2.png";
+import blogCart3 from "../foto/blogFoto/blogCart-3.png";
+import blogCart4 from "../foto/blogFoto/blogCart-4.png";
+import blogCart5 from "../foto/blogFoto/blogCart-5.png";
+import blogCart6 from "../foto/blogFoto/blogCart-6.png";
+
+export const BlogePageData = [
+  {
+    id: 1,
+    img: blogCart1,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+  {
+    id: 2,
+    img: blogCart2,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+  {
+    id: 3,
+    img: blogCart3,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+  {
+    id: 4,
+    img: blogCart4,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+  {
+    id: 5,
+    img: blogCart5,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+  {
+    id: 6,
+    img: blogCart6,
+    title: "Koudetat à la Maison #1 (L'intégrale)",
+    description:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "22 April 2021",
+    comments: 10,
+  },
+];
