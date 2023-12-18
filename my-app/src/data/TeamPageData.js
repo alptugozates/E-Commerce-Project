b1f9eb0fd37cb367243027e3ce611636e22e85bd
alptugozates <omeralptugozates@gmail.com>
2023-12-18 14:54:48 +0300

    teampage data eklendi

---
+++ my-app/src/data/TeamPageData.js
@@ -0,0 +1,117 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import teamUser1 from "../foto/TeamFoto/team-1-user-1.jpg";
+import teamUser2 from "../foto/TeamFoto/team-1-user-2.jpg";
+import teamUser3 from "../foto/TeamFoto/team-1-user-3.jpg";
+import teamUser4 from "../foto/TeamFoto/team-1-user-4.jpg";
+import teamUser5 from "../foto/TeamFoto/team-1-user-5.jpg";
+import teamUser6 from "../foto/TeamFoto/team-1-user-6.jpg";
+import teamUser7 from "../foto/AboutFoto/team-1-user-1.jpg";
+import teamUser8 from "../foto/AboutFoto/team-1-user-2.jpg";
+import teamUser9 from "../foto/AboutFoto/team-1-user-3.jpg";
+
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser1,
+  },
+  {
+    id: 2,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser2,
+  },
+  {
+    id: 3,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser3,
+  },
+  {
+    id: 4,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser4,
+  },
+  {
+    id: 5,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser5,
+  },
+  {
+    id: 6,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser6,
+  },
+  {
+    id: 7,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser7,
+  },
+  {
+    id: 8,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser8,
+  },
+  {
+    id: 19,
+    name: "Username",
+    profession: "Profession",
+    social: {
+      facebook: faFacebook,
+      instagram: faInstagram,
+      twitter: faTwitter,
+    },
+    image: teamUser9,
+  },
+];
