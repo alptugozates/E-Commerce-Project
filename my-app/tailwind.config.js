349fc13714235381f542ed28fe4bdcc52201a468
alptugozates <omeralptugozates@gmail.com>
2023-12-18 22:16:55 +0300

    customization olarak xs 400px olarak ekler

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "400px",
+    },
     extend: {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
