fec3822f147287585d217638dad9816104c43970
alptugozates <omeralptugozates@gmail.com>
2023-12-18 14:55:36 +0300

    TeamPage, ContactPage router'a ekler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,11 @@ import HomePage from "./components/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductListPage from "./components/ProductListPage";
 import BlogPage from "./components/BlogPage";
-
 import ProductPage from "./components/ProductPage";
 import AboutPage from "./components/AboutPage";
+import TeamPage from "./components/TeamPage";
+import ContactPage from "./components/ContactPage";
+
 function App() {
   return (
     <Router>
@@ -24,6 +26,12 @@ function App() {
         </Route>
         <Route path="/about">
           <AboutPage />
+        </Route>
+        <Route path="/team">
+          <TeamPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
         </Route>
       </div>
     </Router>
