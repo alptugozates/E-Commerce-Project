1f6969ee8e85c55ef694d1aec19070eef06781db
alptugozates <omeralptugozates@gmail.com>
2023-12-25 15:50:34 +0300

    actionları olusturur

---
+++ my-app/src/actions/storeAction.js
@@ -0,0 +1,6 @@
+export const SET_SELLER_STORE = "SET_SELLER_STORE";
+
+export const setSellerStore = (sellerStore) => ({
+  type: "SET_SELLER_STORE",
+  payload: sellerStore,
+});
---
+++ my-app/src/actions/userAction.js
@@ -0,0 +1,18 @@
+export const UPDATE_NAME = "UPDATE_NAME";
+export const UPDATE_EMAIL = "UPDATE_EMAIL";
+export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
+
+export const updateName = (name) => ({
+  type: "UPDATE_NAME",
+  payload: name,
+});
+
+export const updateEmail = (email) => ({
+  type: "UPDATE_EMAIL",
+  payload: email,
+});
+
+export const updatePassword = (password) => ({
+  type: "UPDATE_PASSWORD",
+  payload: password,
+});
