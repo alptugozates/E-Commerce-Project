c39bc6e5f31b3af5e823380922e86c73ed8012b9
alptugozates <omeralptugozates@gmail.com>
2023-12-25 15:50:13 +0300

    product Action olusturur

---
+++ my-app/src/actions/productAction.js
@@ -0,0 +1,17 @@
+export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
+export const UPDATE_ACTIVE_PAGE = "UPDATE_ACTIVE_PAGE";
+
+export const fetchProducts = (
+  productList,
+  totalProductCount,
+  pageCount,
+  fetchState
+) => ({
+  type: "FETCH_PRODUCTS",
+  payload: { productList, totalProductCount, pageCount, fetchState },
+});
+
+export const updateActivePage = (activePage) => ({
+  type: "UPDATE_ACTIVE_PAGE",
+  payload: { activePage },
+});
