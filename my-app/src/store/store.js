b8ff7e5b81b317e5a1e554c211c1d8cb0625ab69
alptugozates <omeralptugozates@gmail.com>
2023-12-25 15:51:36 +0300

    redux icin store olusturur combineReducer ile reducerları sarar

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -1,9 +1,25 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+
 import logger from "redux-logger";
+import globalReducer from "../reducers/globalReducer";
+import userReducer from "../reducers/userReducer";
+import productReducer from "../reducers/productReducer";
+import storeReducer from "../reducers/storeReducer";
+import { shoppingCartReducer } from "../reducers/shoppingCartReducer";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(reducer, applyMiddleware(thunk, logger));
+const rootReducer = combineReducers({
+  global: globalReducer,
+  user: userReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer,
+  store: storeReducer,
+});
 
-function reducer() {
-  return;
-}
+const middleware = [thunk, logger];
+
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
