f855314d06cd7f057244cfc4c02be87f5ec8afee
alptugozates <omeralptugozates@gmail.com>
2023-12-26 12:04:10 +0300

    basURL fixed

--- my-app/src/axios/axiosInstance.js
+++ my-app/src/axios/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://workinteck-fe-final.onrender.com",
+  baseURL: "https://workintech-fe-ecommerce.onrender.com",
 });
 export default axiosInstance;
