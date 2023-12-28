37c0ff69db22d2dd53d3a4fd7ce765977fd67725
alptugozates <omeralptugozates@gmail.com>
2023-12-28 20:39:03 +0300

    loginData action'ı olusturur

--- my-app/src/actions/userAction.js
+++ my-app/src/actions/userAction.js
@@ -1,18 +1,13 @@
-export const UPDATE_NAME = "UPDATE_NAME";
-export const UPDATE_EMAIL = "UPDATE_EMAIL";
-export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
+export const LOGGED_IN = "LOGGED_IN";
 
-export const updateName = (name) => ({
-  type: UPDATE_NAME,
-  payload: name,
-});
-
-export const updateEmail = (email) => ({
-  type: UPDATE_EMAIL,
-  payload: email,
-});
-
-export const updatePassword = (password) => ({
-  type: UPDATE_PASSWORD,
-  payload: password,
+export const loginData = ({
+  name,
+  email,
+  password,
+  role,
+  photo,
+  loggedIn,
+}) => ({
+  type: LOGGED_IN,
+  payload: { name, email, password, role, photo, loggedIn },
 });
