34085c4a36e37b3d936eaacf824cff098684fae2
alptugozates <omeralptugozates@gmail.com>
2023-12-29 19:06:21 +0300

    useEffect import eder

--- my-app/src/pages/LoginPage.js
+++ my-app/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import Footer from "../components/Footer";
@@ -14,7 +14,6 @@ import { writeToLocalStorage } from "../reducers/userReducer";
 const LoginPage = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-
   const [login, setLogin] = useState(false);
 
   const {
