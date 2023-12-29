8b4436de9b025d2cb80ba9b12ffe85e0c7455698
alptugozates <omeralptugozates@gmail.com>
2023-12-29 19:06:35 +0300

    loggedOut action ekler

--- my-app/src/actions/userAction.js
+++ my-app/src/actions/userAction.js
@@ -1,5 +1,5 @@
 export const LOGGED_IN = "LOGGED_IN";
-
+export const LOGGED_OUT = "LOGGED_OUT";
 export const loginData = ({
   name,
   email,
@@ -10,4 +10,9 @@ export const loginData = ({
 }) => ({
   type: LOGGED_IN,
   payload: { name, email, password, role, photo, loggedIn },
+});
+
+export const loggedOut = ({ name, loggedIn, email }) => ({
+  type: LOGGED_OUT,
+  payload: { name, email, loggedIn },
 });
