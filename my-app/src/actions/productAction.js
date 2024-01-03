7fef78ad736f0960cf88f69905d7c8fd0ca7f346
alptugozates <omeralptugozates@gmail.com>
2024-01-03 18:30:56 +0300

    thunk action ile products fetch eder

--- my-app/src/actions/productAction.js
+++ my-app/src/actions/productAction.js
@@ -1,3 +1,4 @@
+import axiosInstance from "../axios/axiosInstance";
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 export const UPDATE_ACTIVE_PAGE = "UPDATE_ACTIVE_PAGE";
 
@@ -15,3 +16,35 @@ export const updateActivePage = (activePage) => ({
   type: UPDATE_ACTIVE_PAGE,
   payload: { activePage },
 });
+
+export const fetchProductsData = (category, filter, sort) => {
+  return (dispatch) => {
+    let url = "/products";
+    if (category) {
+      url += `?category=${category}`;
+    }
+    if (filter) {
+      url += `&filter=${filter}`;
+    }
+    if (sort) {
+      url += `&sort=${sort}`;
+    }
+
+    return axiosInstance
+      .get("/products")
+      .then((response) => {
+        dispatch(
+          fetchProducts(
+            response.data.productList,
+            response.data.totalProductCount,
+            response.data.pageCount,
+            "FETCHED"
+          )
+        );
+      })
+
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  };
+};
