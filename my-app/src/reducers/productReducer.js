aef985306022f3f5d455948fc2ded8301c149ef2
alptugozates <omeralptugozates@gmail.com>
2024-01-04 17:59:35 +0300

    case'e göre filter  reducer'ı oluşturur

--- my-app/src/reducers/productReducer.js
+++ my-app/src/reducers/productReducer.js
@@ -1,4 +1,12 @@
-import { FETCH_PRODUCTS, UPDATE_ACTIVE_PAGE } from "../actions/productAction";
+import {
+  FETCH_PRODUCTS,
+  SORT_BY_ALPHABETICAL,
+  SORT_BY_PRICE_HIGH_TO_LOW,
+  SORT_BY_PRICE_LOW_TO_HIGH,
+  SORT_BY_RATING_DESCENDING,
+  UPDATE_ACTIVE_PAGE,
+  SORT_BY_STOCK,
+} from "../actions/productAction";
 
 const initialState = {
   products: [],
@@ -28,6 +36,49 @@ const productReducer = (state = initialState, action) => {
       return {
         ...state,
         activePage: action.payload.activePage,
+      };
+    case SORT_BY_ALPHABETICAL:
+      const sortedAlphabetical = state.products
+        .slice()
+        .sort((a, b) => a.name.localeCompare(b.name));
+      return {
+        ...state,
+        products: sortedAlphabetical,
+      };
+
+    case SORT_BY_RATING_DESCENDING:
+      const sortedByRating = state.products
+        .slice()
+        .sort((a, b) => b.rating - a.rating);
+      return {
+        ...state,
+        products: sortedByRating,
+      };
+
+    case SORT_BY_PRICE_HIGH_TO_LOW:
+      const sortedByPriceHighToLow = state.products
+        .slice()
+        .sort((a, b) => b.price - a.price);
+      return {
+        ...state,
+        products: sortedByPriceHighToLow,
+      };
+
+    case SORT_BY_PRICE_LOW_TO_HIGH:
+      const sortedByPriceLowToHigh = state.products
+        .slice()
+        .sort((a, b) => a.price - b.price);
+      return {
+        ...state,
+        products: sortedByPriceLowToHigh,
+      };
+    case SORT_BY_STOCK:
+      const sortedByStock = state.products
+        .slice()
+        .sort((a, b) => a.stock - b.stock);
+      return {
+        ...state,
+        products: sortedByStock,
       };
     default:
       return state;
