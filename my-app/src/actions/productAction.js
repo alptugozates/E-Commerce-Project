2aae41d9cc5f1a0f8199fcba8989bec36450f937
alptugozates <omeralptugozates@gmail.com>
2024-01-04 17:54:26 +0300

    filtrelenecek kriterlerin action'ı olusturur

--- my-app/src/actions/productAction.js
+++ my-app/src/actions/productAction.js
@@ -1,7 +1,11 @@
 import axiosInstance from "../axios/axiosInstance";
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 export const UPDATE_ACTIVE_PAGE = "UPDATE_ACTIVE_PAGE";
-
+export const SORT_BY_ALPHABETICAL = "SORT_BY_ALPHABETICAL";
+export const SORT_BY_RATING_DESCENDING = "SORT_BY_RATING_DESCENDING";
+export const SORT_BY_PRICE_HIGH_TO_LOW = "SORT_BY_PRICE_HIGH_TO_LOW";
+export const SORT_BY_PRICE_LOW_TO_HIGH = "SORT_BY_PRICE_LOW_TO_HIGH";
+export const SORT_BY_STOCK = "SORT_BY_STOCK";
 export const fetchProducts = (
   products,
   totalProductCount,
@@ -16,6 +20,25 @@ export const fetchProducts = (
 export const updateActivePage = (activePage) => ({
   type: UPDATE_ACTIVE_PAGE,
   payload: { activePage },
+});
+
+export const sortByAlphabetical = () => ({
+  type: SORT_BY_ALPHABETICAL,
+});
+
+export const sortByRatingDescending = () => ({
+  type: SORT_BY_RATING_DESCENDING,
+});
+
+export const sortByPriceHighToLow = () => ({
+  type: SORT_BY_PRICE_HIGH_TO_LOW,
+});
+
+export const sortByPriceLowToHigh = () => ({
+  type: SORT_BY_PRICE_LOW_TO_HIGH,
+});
+export const sortByStock = () => ({
+  type: SORT_BY_STOCK,
 });
 
 export const fetchProductsData = (category, filter, sort) => {
